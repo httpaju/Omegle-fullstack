bdda03a0eb7d6c387ecfacd5b9d190bbbf64cd40
httpaju <165481606+httpaju@users.noreply.github.com>
2025-03-16 18:19:33 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,10 +5,6 @@ export default defineConfig({
     host: "0.0.0.0",
     port: process.env.PORT || 5173,
     strictPort: true,
-    allowedHosts: ["all"], // Allows all hosts
-  },
-  preview: {
-    port: process.env.PORT || 4173,
-    allowedHosts: ["all"], // Ensures preview mode works too
+    allowedHosts: ["strangercam.onrender.com"],
   }
 });
